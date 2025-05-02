913ef26df824d62f02f376e2c2ddf0a53c9288c3
lizc <lizc@fists.cn>
2025-05-02 15:06:38 +0800

    Initial Commit

---
+++ Focus/FocusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FocusApp.swift
+//  Focus
+//
+//  Created by lizc on 2025/5/2.
+//
+
+import SwiftUI
+
+@main
+struct FocusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
